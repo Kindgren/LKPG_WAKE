f843fea91077ad19c8a90432f5be6f35be870c1e
gustavkindgren <gustav.kindgren@scila.se>
2025-06-17 20:46:27 +0200

    Fix minor casing error causing the build to fail

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/Aura/'; // ✅ new theme package
+import Aura from '@primeuix/themes/aura/'; 
 import { router } from './router';
 import 'primeicons/primeicons.css'
 
