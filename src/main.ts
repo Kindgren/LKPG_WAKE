2c839680fca508c6481a5cc29310d11ce97f2ccc
gustavkindgren <gustav.kindgren@scila.se>
2025-07-04 17:03:27 +0200

    Another go at to font sizes

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,24 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura/'; 
-import { router } from './router';
-import 'primeicons/primeicons.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura/";
+import "./style.css";
+import { router } from "./router";
+import "primeicons/primeicons.css";
 
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Drawer from "primevue/drawer";
+import Menu from "primevue/menu";
+import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persistedstate";
 
-import Button from 'primevue/button'
-import Card from 'primevue/card'
-import Drawer from 'primevue/drawer'
-import Menu from 'primevue/menu'
-import { createPinia } from 'pinia'
-import piniaPersist from 'pinia-plugin-persistedstate'
-
-
-const pinia = createPinia()
-pinia.use(piniaPersist)
+const pinia = createPinia();
+pinia.use(piniaPersist);
 
 const app = createApp(App);
 
-
-app.use(pinia) 
+app.use(pinia);
 app.use(router);
 app.use(PrimeVue, {
   theme: {
@@ -28,10 +26,9 @@ app.use(PrimeVue, {
   },
 });
 
-app.component('Button', Button);
-app.component('Card', Card);
-app.component('Drawer', Drawer);
-app.component('Menu', Menu);
+app.component("Button", Button);
+app.component("Card", Card);
+app.component("Drawer", Drawer);
+app.component("Menu", Menu);
 
-
-app.mount('#app');
+app.mount("#app");
